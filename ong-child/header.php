ce0132e8ca4fd200341c1c6ec8bf2c98bcdf9147
Guilherme de Favere <guilherme.favere@gmail.com>
2018-09-15 14:26:38 -0300

    Header

--- ong-child/header.php
+++ ong-child/header.php
@@ -1,3 +1,18 @@
 <header class="header">
-    <h2>HEADER</h2>
+    <h1 class="hide">ONG Projeto Emanuel</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-10">
+                <ul class="phones">
+                    <li><a href="tel:1936290602" title="(19) 3629-0602">(19) 3629-0602</a></li>
+                    <li><a href="tel:1936290603" title="(19) 3629-0603">(19) 3629-0603</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-2">
+                <ul class="social-networks">
+                    <li><a href="https://www.facebook.com/Projetoemanuel1/" target="_blank" title="ONG Projeto emanuel facebook"></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </header>
