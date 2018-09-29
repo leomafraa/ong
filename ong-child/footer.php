a3fc88bb15078aa01751e2ed3bdff93b12eb312d
Guilherme de Favere <guilherme.favere@gmail.com>
2018-09-29 16:31:14 -0300

    Footer

--- ong-child/footer.php
+++ ong-child/footer.php
@@ -1,5 +1,41 @@
 <footer class="footer">
-    <h2>FOOTER</h2>
+    <div class="contact-info">
+        <div class="container">
+            <ul class="row">
+                <li class="col-sm-3">
+                    <h2>Rua: Leonel Pântano 290 - Jardim Dona Regina, Santa Bárbara D'Oeste - São Paulo, Brasil</h2>
+                </li>
+                <li class="col-sm-3">
+                    <a href="tel:1936290602" title="(19) 3629-0602">
+                        <h2>(19) 3629-0602</h2>
+                    </a>
+                    <a href="tel:1936290603" title="(19) 3629-0603">
+                        <h2>(19) 3629-0603</h2>
+                    </a>
+                </li>
+                <li class="col-sm-3">
+                    <a href="mailto:ong@projetoemanuel.org.br" title="ong@projetoemanuel.org.br">
+                        <h2>ong@projetoemanuel.org.br</h2>
+                    </a>
+                </li>
+                <li class="col-sm-3">
+                    <h2>Segunda a Sexta-feira 19h às 21h30, e Sábado 08h às 18h</h2>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8">
+                    <p>Copyright 2018 © Ong Projeto Emanuel - Todos os direitos reservados</p>
+                </div>
+                <div class="col-sm-4">
+                    <p class="ziny">Desenvolvido com <i class="glyphicon glyphicon-heart"></i> por <a href="http://agenciaziny.com.br/" target="_blank" title="Agência Ziny">Agência Ziny</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
 <link href="https://fonts.googleapis.com/css?family=Pacifico|Raleway:400,700" rel="stylesheet">
 <?php wp_footer(); ?>
